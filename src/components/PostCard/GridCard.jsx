e400e76b2d25e0aafe3f849463c7a07cd9720c1d
Hangeony <geonhui95@naver.com>
2023-03-30 23:46:56 +0900

    230330: Home Component 분리

---
+++ src/components/PostCard/GridCard.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+import PostCard from ".";
+import NavBar from "../NavBar";
+
+function GraidCard(){
+    return(
+        <>
+        <NavBar/>
+
+        <MarginRL>
+            <FlexMT>
+                <MainFlex>
+                    <GridCard>
+                        {Array.from({ length: 11 }).map((_, index) => (
+                            <PostCard key={`-${index}`} />
+                        ))}
+                        {/* <PostCard /> */}
+                    </GridCard>
+                </MainFlex>
+            </FlexMT>
+        </MarginRL>
+    </>
+);
+};
+
+
+const MarginRL = styled.div`
+    width: 1728px;
+    margin-left: auto;
+    margin-right: auto;
+    
+    @media (max-width: 1056px) {
+        width: calc(100% - 2rem);
+    }
+    @media (max-width: 1440px) {
+        width: 1024px;
+    }
+    @media (max-width: 1919px) {
+        width: 1376px;
+    }
+`;
+const FlexMT = styled.div`
+    display: flex;
+    margin-top: 5rem;
+`;
+
+const MainFlex = styled.main`
+    flex: 1 1 0%;
+`;
+
+const GridCard = styled.div`
+    display: flex;
+    margin: -1rem;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    
+    @media(max-width:767px) {
+        /* margin: 0; */
+    }
+`;
+
+export default GraidCard;
