9543a2932a5a7dde256883031f0d910dfdd16e4f
Hangeony <geonhui95@naver.com>
2023-04-24 00:20:58 +0900

    230424: 어드민페이지 연동 및 파일위치 수정

---
+++ src/components/Admin/index.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import NavBar from "../NavBar";
+import {
+  MenuFoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Button, Layout, Menu, theme } from "antd";
+
+function Admin() {
+  const { Header, Sider, Content } = Layout;
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <>
+      <NavBar />
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            items={[
+              {
+                key: "1",
+                icon: <UserOutlined />,
+                label: "nav 1",
+              },
+              {
+                key: "2",
+                icon: <VideoCameraOutlined />,
+                label: "nav 2",
+              },
+              {
+                key: "3",
+                icon: <UploadOutlined />,
+                label: "nav 3",
+              },
+            ]}
+          />
+        </Sider>
+        <Layout>
+          <Header
+            style={{
+              padding: 0,
+              background: colorBgContainer,
+            }}
+          >
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: "16px",
+                width: 64,
+                height: 64,
+              }}
+            />
+          </Header>
+          <Content
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: "100vh",
+              background: colorBgContainer,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </>
+  );
+}
+export default Admin;
